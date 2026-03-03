d3e358dbea84690b1769b2a0e784d5330a58f298
mwarren306 <mattwarren306@gmail.com>
2026-03-03 12:51:22 -0600

    Add Github Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://mwarren306.github.io',
+  base: '/paradyne-site',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
